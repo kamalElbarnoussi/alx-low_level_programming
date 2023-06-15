21e977f051392ec94536f2e207c09962c6eb5196
kamalElbarnousi <kamalgskipol@gmail.com>
2023-06-15 15:00:39 -0700

    C-HELLO_WORLD

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
