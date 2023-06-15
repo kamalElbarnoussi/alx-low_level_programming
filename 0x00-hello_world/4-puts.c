86974a17be19d7eeb9b064bf63959a37ca6ca276
kamalElbarnousi <kamalgskipol@gmail.com>
2023-06-15 14:12:09 -0700

    C-HELLO_WORLD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/* this is  a c program using puts*/
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
