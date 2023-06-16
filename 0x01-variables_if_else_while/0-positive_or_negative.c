587e0d31d21db17a82d2a6d51474a2b061761443
kamalElbarnousi <kamalgskipol@gmail.com>
2023-06-16 13:10:41 -0700

    C_variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,61 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main -Entry point
+*
+*Return:Always 0 (Success) 
+**/
+
 int main(void)
 {
-/* to generate a random number */
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* to detrmine the sign of the number */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
-}
+	int n;
+	srand(time(0);
+	n = rand()-RAND-MAX/2;
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+	if (n>0) {
+	printf("%d is positive\n",n);
+	}
+	if (n<0) {
+	printf("%d is negative\n",n);
+	} 
+	if (n==0) {
+	printf("%d is zero\n",n);
+	}
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-/* to generate a random number */
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* to detrmine the sign of the number */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
+	return (0);
 }
