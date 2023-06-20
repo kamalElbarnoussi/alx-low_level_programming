2ce93e56673f5335d1048b2cd289013c2f988b6e
kamalElbarnousi <kamalgskipol@gmail.com>
2023-06-20 11:13:49 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - check the char 
+ * _islower - check the char
  * @c: is the char to be checked
- * Return: 1 if char, otherwise 0.
+ * Return: 1 if char, otherwise 0
  */
 
 int _islower(int c)
