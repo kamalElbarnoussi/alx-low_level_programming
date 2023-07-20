d1956110f05f5e0c708aa3eca0c381d0e1252cf2
kamalElbarnousi <kamalgskipol@gmail.com>
2023-07-20 03:57:15 -0700

    C-Variadic_functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef _variadic_functions_
+#define _variadic_functions_
+
+#include<stdio.h>
+#include<stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
